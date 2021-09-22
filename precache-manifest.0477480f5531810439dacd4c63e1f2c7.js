e0c28dec59962c9016f38366ba985035a98eb4f9
jassi73 <jessparihar73@gmail.com>
2021-09-22 16:52:39 +0530

    Updates

---
+++ precache-manifest.0477480f5531810439dacd4c63e1f2c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf68f70c78b41f7b85f4b664c7676b8",
+    "url": "/advice-quotes/index.html"
+  },
+  {
+    "revision": "36d27a932fdb2a591a37",
+    "url": "/advice-quotes/static/css/main.dc535bf4.chunk.css"
+  },
+  {
+    "revision": "38d3f1e46957e1a145d5",
+    "url": "/advice-quotes/static/js/2.1c40d22c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/advice-quotes/static/js/2.1c40d22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d27a932fdb2a591a37",
+    "url": "/advice-quotes/static/js/main.4e0fb482.chunk.js"
+  },
+  {
+    "revision": "6b381c1c743aad8aa64f",
+    "url": "/advice-quotes/static/js/runtime-main.091b9ad4.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/advice-quotes/static/media/city.91c109fc.jpg"
+  }
+]);
